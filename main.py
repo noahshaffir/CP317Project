6f50eb85b39075562677485077bfeb24316c6f5e
noahshaffir <noahshaffir@gmail.com>
2023-03-22 13:24:29 -0400

    working on layout

--- main.py
+++ main.py
@@ -16,9 +16,25 @@ def load_data(ticker1, ticker2, start,end):
 def plot(data, indicators, sync_axis=None):
     pass
 
-def on_click(ticker1, ticker2, start, end, indicators):
+def on_button_click(ticker1, ticker2, start, end, indicators):
     pass
 
-stock1_test = TextInput(title="Stock 1")
-stock2_test = TextInput(title="Stock 2")
-date_picker_from= DatePicker(title="Start Date", value="2022-04-07",min_date=datetime.datetime.now().strftime("%Y-%m-%d"))
+
+def display_about():
+    print("implement later")
+
+stock1_text = TextInput(title="Stock 1")
+stock2_text = TextInput(title="Stock 2")
+date_picker_from= DatePicker(title="Start Date", value="2019-01-01",min_date="2000-01-01", max_date=datetime.datetime.now().strftime("%Y-%m-%d"))
+date_picker_to= DatePicker(title="End Date", value="2020-01-01",min_date="2000-01-01", max_date=datetime.datetime.now().strftime("%Y-%m-%d"))
+
+indicator_choice = MultiChoice(options=["100 days' average", "30 days' average", "Linear regression"])
+
+load_button=Button(label="Display results")
+load_button.on_click(lambda: on_button_click(stock1_text.value,stock2_text.value,date_picker_from.value,date_picker_to.value,indicator_choice.value,load_button))
+
+layout = row(stock1_text,stock2_text,date_picker_from,date_picker_to,indicator_choice, load_button)
+
+curdoc().clear()
+curdoc().add_root(layout)
+
